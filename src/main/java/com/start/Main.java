117bc0e49ad28817d633b1fd65440da2abe016d8
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-04-20 07:43:38 +0200

    added java Spring project starter

---
+++ src/main/java/com/start/Main.java
@@ -0,0 +1,9 @@
+package com.start;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Doctor doctor = new Doctor();
+        doctor.assist();
+    }
+}
